ed72ef12156b0da47ae7664772b7ca4814ab741c
Mburg03 <mburgosgit003@gmail.com>
2024-02-20 15:21:00 -0600

    full web page with everything ok

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MenuListView.as_view(), name='home')
+    path('', views.MenuListView.as_view(), name='home'),
+    path('item/<int:pk>/', views.MenuItemDetailView.as_view(), name='menu_item'),
+    path('about/', views.AboutView.as_view(), name='about'),
 ]
